13b4a1a0bf32a9020cd160316dd6a82c4635982a
Fábio Ribeiro <109528947+fabi0ribeir0@users.noreply.github.com>
2022-10-20 11:45:17 -0300

    repetição

---
+++ Aula13/ambiente.js
@@ -0,0 +1,27 @@
+var cont = 1
+ do {
+    console.log(cont)
+    cont++
+} while (cont <= 6)
+
+/* var cont = 1
+while (cont <= 6) {
+    console.log(cont)
+    cont++
+    */
+
+
+
+
+
+
+
+/*
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+*/
--- pizza/teste.js
+++
@@ -1,6 +0,0 @@
-setTimeout(function() {
-    console. log("Alguma coisa")
-    }, 1000);
-    setTimeout(function() {
-        console. log("outra coisa")
-        }, 2000);
